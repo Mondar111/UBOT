760bcd6a68d312b509edfd43685b8e82272d993b
Mondar111 <86989223+Mondar111@users.noreply.github.com>
2021-07-19 07:07:29 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⭐RAM-UBOT⭐ 🔥 V7.0 [AKTIF NGENTOT!!!!]")
+    f"⛩️👹 𝑴𝑶𝑵-𝑼𝑺𝑬𝑹𝑩𝑶𝑻 👹⛩️ 💥 V8.0 [AKTIF SAYANG!!!!]")
 
 
 if len(argv) not in (1, 3, 4):
