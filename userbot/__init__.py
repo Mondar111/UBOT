ffaf9d2edffdaf0ad102f9dbe0ca3d2c3f71c660
Mondar111 <86989223+Mondar111@users.noreply.github.com>
2021-07-21 07:52:41 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -233,7 +233,7 @@ OWNER_BOT = os.environ.get(
 
 # Last.fm Module
 BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
-DEFAULT_BIO = os.environ.get("DEFAULT_BIO") or "⛩️👹 𝑴𝑶𝑵-𝑼𝑺𝑬𝑹𝑩𝑶𝑻 👹⛩️"
+DEFAULT_BIO = os.environ.get("DEFAULT_BIO") or "⛩️𝑴𝑶𝑵-𝑼𝑺𝑬𝑹𝑩𝑶𝑻⛩️"
 
 LASTFM_API = os.environ.get("LASTFM_API", None)
 LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
